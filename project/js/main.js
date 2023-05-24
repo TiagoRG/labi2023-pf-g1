dc8cae18ed0d342775ecfb1df905aa3a95bb7b0c
Bruno Borlido <116981631+brunombp@users.noreply.github.com>
2023-05-24 18:11:42 +0100

    --> Criação da página "main.html" (#7)
    - Criação do menu principal com botões para redirecionar para as outras páginas;
    - Criação de uma área de destaques (#7)
    
    Co-authored-by: brunombpereira <brunoborlido@gmail.com>
    Co-authored-by: TiagoRG <tiago.rgarcia@ua.pt>

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,14 @@
+let menuOpen = false; // variável para rastrear o estado do menu
+
+function toggleNav() {
+    if (!menuOpen) {
+        openNav();
+    } else {
+        closeNav();
+    }
+    menuOpen = !menuOpen; // inverta o estado do menu
+}
+
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
