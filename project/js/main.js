71be4287c9a019a626d372fe2fd9ea660e3ae389
Bruno Borlido <116981631+brunombp@users.noreply.github.com>
2023-05-24 22:38:13 +0100

    --> Atualização da página principal (#9)
    Co-authored-by: brunombpereira <brunoborlido@gmail.com>

--- project/js/main.js
+++ project/js/main.js
@@ -12,9 +12,27 @@ function toggleNav() {
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mainFooter").style.marginLeft = "250px";
 }
 
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("mainFooter").style.marginLeft= "0";
 }
+
+window.addEventListener('click', function(e) {
+    if (!document.getElementById('mySidebar').contains(e.target) &&
+        !document.querySelector('.openbtn').contains(e.target) &&
+        menuOpen) {
+        closeNav();
+        menuOpen = false;
+    }
+});
+
+window.addEventListener('scroll', function() {
+    if (menuOpen) {
+        closeNav();
+        menuOpen = false;
+    }
+});
