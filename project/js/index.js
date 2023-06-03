521dc3ec39b9f92e552e3509fbc9630da7453649
Rúben Gomes <116815718+RubenCGomes@users.noreply.github.com>
2023-06-03 23:07:57 +0100

    FIxed index and added fadeIn/fadeOut animations (#13)

--- project/js/index.js
+++ project/js/index.js
@@ -1,21 +1,32 @@
-var x = document.getElementById("login");
-var y = document.getElementById("register");
-var z = document.getElementById("btn");
+var loginId = document.getElementById("login");
+var registerId = document.getElementById("register");
+var btnId = document.getElementById("btn");
 
 function register(){
-    x.style.left = "-120%";
-    y.style.left = "8%";
-    z.style.left = "50%";
-    y.style.up = "50%";
+    loginId.style.left = "-120%";
+    registerId.style.left = "8%";
+    btnId.style.left = "50%";
 }
 function login(){
-    x.style.left = "8%";
-    y.style.left = "120%";
-    z.style.left = "0%";
+    loginId.style.left = "8%";
+    registerId.style.left = "120%";
+    btnId.style.left = "0%";
 }
+
 function showPopup() {
-     document.getElementById("popup").style.display = "block";
+    document.getElementById("popup1").style.animation = "fadeIn 0.5s";
+    document.getElementById("popup1").style.opacity = "1";
+    document.getElementById("popup1").style.display = "block";
 }
+
+function hidePopup() {
+    document.getElementById("popup1").style.animation = "fadeOut 0.5s";
+    document.getElementById("popup1").style.opacity = "0";
+    setTimeout(() => {document.getElementById("popup1").style.display = "none"}, 500) // wait for 500ms
+
+
+}
+
 function done() { function done() {
     document.getElementById("popup").style.display = "none";
     var password = document.getElementById("pass").value;
