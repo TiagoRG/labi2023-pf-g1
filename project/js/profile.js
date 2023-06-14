d4b86d403be94c0dd0e0639a07107a3679aadafc
tiagof-pk <116829313+tiagof-pk@users.noreply.github.com>
2023-06-14 18:36:48 +0100

    Profile added (#19)

---
+++ project/js/profile.js
@@ -0,0 +1,27 @@
+$('.nav ul li').click(function(){
+
+    $(this).addClass("active").siblings().removeClass('active');
+})
+
+const tabBtn = document.querySelectorAll('.nav ul li');
+const tab = document.querySelectorAll('.tab');
+
+function tabs(panelIndex){
+    tab.forEach(function(node){
+        node.style.display = 'none';
+
+    });
+    tab[panelIndex].style.display = 'block';
+}
+tabs(0);
+function showPopup() {
+     document.getElementById("popup").style.display = "block";
+}
+
+function done() { function done() {
+    document.getElementById("popup").style.display = "none";
+    var password = document.getElementById("pass").value;
+
+    //DO STUFF WITH PASSWORD HERE
+}
+}
